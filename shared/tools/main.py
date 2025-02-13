0ab1a3022b425a8cefc6558b5050040df680c29d
xxcosita3czxx <kokosove18@gmail.com>
2025-02-13 22:54:13 +0100

    making logger work

--- shared/tools/main.py
+++ shared/tools/main.py
@@ -4,7 +4,18 @@ import click # type: ignore
 import logging
 import yaml # type: ignore
 import sys
-logger = logging.Logger("main",logging.INFO)
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        if record.levelno == logging.INFO:
+            return record.getMessage()  # No prefix for INFO
+        return f"[{record.levelname}] {record.getMessage()}"  # Prefix for others
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+for handler in logger.handlers:
+    handler.setFormatter(CustomFormatter())
 
 # Initialize the main CLI group
 @click.group()
